6c95a870f2cef65fd246725819b8939ec322fbe5
mobin <mobinfalahpor100@gmail.com>
2016-02-14 08:30:35 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -223,29 +223,11 @@ function create_config( )
     "leave_ban"
     
     },
-    sudo_users = {147049730},--Sudo users
+    sudo_users = {147049730,85831686},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+    about_text = [[black life tm v2 - Open Source
+An advance Administration bot based on yagop/telegram-bot
 ]],
     help_text_realm = [[
 Realm Commands:
